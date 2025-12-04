235de8bb64e52491b861a4b74acee2d77b5d743a
Rizal Maulana <xjunngoat1@gmail.com>
2025-12-04 07:30:05 +0700

    percobaan 1

--- rekursif/Percobaan125.java
+++ rekursif/Percobaan125.java
@@ -8,7 +8,7 @@ public class Percobaan125 {
         if (n == 0) {
             return 1; // base case
         } else {
-            return (n * faktorialRekursif(n - 1)); // recursive call
+            return (n * faktorialRekursif(n - 1));
         }
     }
 
